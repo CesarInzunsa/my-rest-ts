dae862650589b389859dd51cb0d810879d79fad5
Cesar Inzunsa <cesarinzunsa@hotmail.com>
2025-02-23 12:10:10 -0700

    git init

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import config from './src/config/config'
+
+// Init dotenv
+dotenv.config()
+
+// Create express app
+const app = express()
+
+// Middlewares
+app.use(express.json())
+
+// Set configs
+app.set('port', config.PORT)
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${config.PORT}`)
+})
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export default {
+  HOTS: process.env.HOST ?? '127.0.0.1',
+  PORT: process.env.PORT ?? 3000,
+  API_PREFIX: process.env.API_PREFIX ?? '/api'
+}
