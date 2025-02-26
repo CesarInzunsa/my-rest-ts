be610005423efd1afd0f71355ffdc2aa18916e52
Cesar Inzunsa <cesarinzunsa@hotmail.com>
2025-02-26 15:58:46 -0700

    feat: integrate Winston logger for improved logging and add environment-specific .env files

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import config from './src/config/config'
-
-// Init dotenv
-dotenv.config()
+import winston from 'winston'
+import { MongoDB } from 'winston-mongodb'
 
 // Create express app
 const app = express()
@@ -14,6 +12,40 @@ app.use(express.json())
 // Set configs
 app.set('port', config.PORT)
 
+// Configure Winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: 'HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      const environment = process.env.NODE_ENV || 'ENV NOT SET'
+      return `[${level}] ${timestamp} [${environment}] ${message}`
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new MongoDB({
+      db: process.env.MONGODB_URI || 'mongodb://localhost:27017/logs',
+      collection: 'log',
+      level: 'info'
+    })
+  ]
+})
+
+// Use logger in express
+app.use((req, _res, next) => {
+  logger.info(`${req.method} ${req.url}`)
+  next()
+})
+
 app.listen(app.get('port'), () => {
-  console.log(`Server is running on port ${config.PORT}`)
+  const serverInfo = {
+    name: 'My RESTful API Server',
+    host: config.HOST,
+    port: app.get('port'),
+    apiPrefix: config.API_PREFIX
+  }
+  const message = `${serverInfo.name} is running on http://${serverInfo.host}:${serverInfo.port}${serverInfo.apiPrefix}`
+  logger.info(message)
 })
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
 export default {
-  HOTS: process.env.HOST ?? '127.0.0.1',
+  HOST: process.env.HOST ?? '127.0.0.1',
   PORT: process.env.PORT ?? 3000,
-  API_PREFIX: process.env.API_PREFIX ?? '/api'
+  API_PREFIX: process.env.API_PREFIX ?? '/api',
+  CONNECTION_STRING: process.env.CONNECTION_STRING ?? 'mongodb://localhost:27017',
+  DATABASE: process.env.DATABASE ?? 'test'
 }
